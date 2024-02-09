220efb5ee2b769d1ce420ee46847263c831f18a5
Michael Etokakpan <imohgenius@yahoo.com>
2024-02-09 14:47:02 +0100

    fallback

--- script.js
+++ script.js
@@ -5,26 +5,34 @@ const customerData = [
     { time: "2.00pm", name: "tony", task: "This task", email: "anthony@home.org" },
   ];
 
-const request = indexedDB.open("MyTestDatabase");
-request.onerror = (event) => {
-  console.error("Why didn't you allow my web app to use IndexedDB?!");
-};
-request.onupgradeneeded  = (event) => {
-  db = event.target.result;
-
+// const request = window.indexedDB.open("MyTestDatabase");
+// request.onerror = (event) => {
+//   console.error("Why didn't you allow my web app to use IndexedDB?!");
+//   alert('Error loading database.')
+// };
+// request.onupgradeneeded  = (event) => {
+//   db = event.target.result;
+//   db.onerror = (event) => {
+//     alert('Error loading database.');
+//   };
   
-  const objectStore= db.createObjectStore("todo",{ autoIncrement: true });
+//   const objectStore= db.createObjectStore("todo",{ autoIncrement: true });
 
-  customerData.forEach((customer)=>{
-    objectStore.add(customer)
-  });
+//   customerData.forEach((customer)=>{
+//     objectStore.add(customer)
+//   });
 
-  let newStore= db.transaction(["todo"], "readwrite").objectStore("todo");
-  console.log(newStore);
-  customerData.forEach((customer)=>{
-    newStore.add(customer)
-  });
-};
+//   let newStore= db.transaction(["todo"], "readwrite").objectStore("todo");
+//   console.log(newStore);
+//   customerData.forEach((customer)=>{
+//     newStore.add(customer)
+//   });
+// };
+
+
+
+
+
 
 // const DBOpenRequest = indexedDB.open("MyTestDatabase");
 
@@ -45,11 +53,14 @@ let button= document.getElementById("submit");
 let todoText= document.getElementById("todo");
 let list= document.getElementById("list");
 let li = document.createElement("li");
-// let tr = document.createElement("tr");
+let delButtons = document.querySelectorAll('.delete');
 
-//  tr.setHTML("<tr><th scope='row'>1</th><td>Buy groceries for next week</td><td>In progress</td><td><button type='submit' class='btn btn-danger'>Delete</button><button type='submit' class='btn btn-success ms-1'>Finished</button></td></tr>");
+button.addEventListener("click", fetch);
 
-button.addEventListener("click", ()=> {
+
+
+
+function fetch (){
 
   const DBOpenRequest = indexedDB.open("MyTestDatabase");
 
@@ -65,54 +76,114 @@ DBOpenRequest.onsuccess = (event) => {
   newStore.add(newData);
 
 
-  let objectArray = db.transaction('todo').objectStore('todo').getAll();
+  let objectArray = db.transaction('todo').objectStore('todo').openCursor();
 
-  objectArray.onsuccess =()=>{
-
+  objectArray.onsuccess =(event)=>{
     list.textContent='';  
     let no = 1;
-  objectArray.result.forEach((item)=>{
-   
-    // let newli= document.createElement("li");
-    let tr = document.createElement("tr");
+
+
+    let cursor = event.target.result;
+    if (cursor) {
+        let key = cursor.primaryKey;
+        let value = cursor.value;
+        let tr = document.createElement("tr");
 
     tr.setHTML("<tr><th scope='row'>1</th><td>Buy groceries for next week</td><td>In progress</td><td><button type='submit' class='btn btn-danger'>Delete</button><button type='submit' class='btn btn-success ms-1'>Finished</button></td></tr>");
 
-    tr.getElementsByTagName('td')[0].textContent= item.task;
-    tr.getElementsByTagName('th')[0].textContent= no;
-    no++;
+        tr.getElementsByTagName('td')[0].textContent= cursor.value.task;
+        tr.getElementsByTagName('th')[0].textContent= no;
+        no++;
+        list.appendChild( tr);
+        console.log(key, value, cursor.key);
+        cursor.continue();
+    }
+    else {
+        // no more results
+    }
+
+    
+  // objectArray.result.forEach((item)=>{
+   
+    // let newli= document.createElement("li");
+    // let tr = document.createElement("tr");
+
+    // tr.setHTML("<tr><th scope='row'>1</th><td>Buy groceries for next week</td><td>In progress</td><td><button type='submit' class='btn btn-danger'>Delete</button><button type='submit' class='btn btn-success ms-1'>Finished</button></td></tr>");
+
+    // tr.getElementsByTagName('td')[0].textContent= item.task;
+    // tr.getElementsByTagName('th')[0].textContent= no;
+    // no++;
 
     // newli.textContent= item.task;
-  list.appendChild( tr);
+  // list.appendChild( tr);
   
- });
+//  });
 }
 
 };
-});
+}
+
+
+function deleted() {
+
+let temptr=this.closest('tr');
+let remove = db
+  .transaction(["todo"], "readwrite")
+  .objectStore("todo")
+  .delete(parseInt(temptr.dataset.value));
+remove.onsuccess = (event) => {
+  alert(temptr.dataset.value);
+  console.log(temptr.dataset.value);
+  temptr.remove();
+};
+
+}
 
 const DBOpenRequest = indexedDB.open("MyTestDatabase");
+
+DBOpenRequest.onupgradeneeded = (event) => {
+  db = event.target.result;
+
+  db.transaction(["todo"], "readwrite").objectStore("todo");
+}
+
 
 DBOpenRequest.onsuccess = (event) => {
 db = DBOpenRequest.result;
 
 
-  let objectArray = db.transaction('todo').objectStore('todo').getAll();
+  let objectArray = db.transaction('todo').objectStore('todo').openCursor();
+  let no = 1;
+  objectArray.onsuccess =(event)=>{
+    // list.textContent='';  
+    
 
-  objectArray.onsuccess =()=>{
 
-    let no = 1;
-  objectArray.result.forEach((item)=>{
-    let tr = document.createElement("tr");
+    let cursor = event.target.result;
+    if (cursor) {
+        let key = cursor.primaryKey;
+        let value = cursor.value;
+        let tr = document.createElement("tr");
 
-    tr.setHTML("<tr><th scope='row'>1</th><td>Buy groceries for next week</td><td>In progress</td><td><button type='submit' class='btn btn-danger'>Delete</button><button type='submit' class='btn btn-success ms-1'>Finished</button></td></tr>");
-
-    tr.getElementsByTagName('td')[0].textContent= item.task;
-    tr.getElementsByTagName('th')[0].textContent= no;
-    no++;
-    // newli.textContent= item.task;
-  list.appendChild( tr);
+    tr.innerHTML="<tr><th scope='row'>1</th><td>Buy groceries for next week</td><td>In progress</td><td><button type='submit' class='btn btn-danger'>Delete</button><button type='submit' class='btn btn-success ms-1 delete '>Finished</button></td></tr>";
+        tr.dataset.value= key;
+        tr.getElementsByTagName('td')[0].textContent= cursor.value.task;
+        tr.getElementsByTagName('th')[0].textContent= no;
+        // console.log(tr, tr.getElementsByTagName('th')[0]);
+        no++;
+       list.appendChild(tr);
+        // console.log(value, cursor.key);
+        cursor.continue();
+    }
+    delButtons = document.querySelectorAll('.delete');
+    delButtons.forEach((btn)=>{
+      btn.addEventListener('click', deleted);
+      }
+      );
+    
   
- });
 }
+
 }
+
+
